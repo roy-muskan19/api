dd2b59b665d32d855d5972f4266a3ec790c2ddc3
roy-muskan19 <muskkaannn.19@gmail.com>
2026-06-27 15:53:00 +0000

    Remove relative import dot from main.py

--- Project 5/TodoApp/main.py
+++ Project 5/TodoApp/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, status
-from .models import Base
+from models import Base
 from .database import engine
 from .routers import auth, todos, admin, users
 from fastapi.staticfiles import StaticFiles
